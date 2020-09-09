5e95a813a68b48ecf692c9cdfae47806269eeb90
ClaFiore <claborghini@gmail.com>
2020-09-09 16:26:50 -0400

    updated controller UserHandbags

--- app/controllers/user_handbags_controller.rb
+++ app/controllers/user_handbags_controller.rb
@@ -4,6 +4,11 @@ class UserHandbagsController < ApplicationController
         user_handbags = UserHandbag.all 
         render json: user_handbags
     end
+
+    def show
+        user_handbag = UserHandbag.find(params[:id])
+        render json: user_handbag
+    end
     
     def create
         user_handbag = UserHandbag.new(user_id: params[:user_id], handbag_id: params[:handbag_id] )
@@ -12,7 +17,7 @@ class UserHandbagsController < ApplicationController
     end
 
     def destroy
-        user_handbag = UserHandbag.find_by(id: params(:id))
+        user_handbag = UserHandbag.find(params[:id])
         user_handbag.destroy
         render json: 'UserHandbag destroyed'
     end
